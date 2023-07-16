d19e8974c38a2f5fdcfc259e95c929fde71965a5
Ahmed Hany <ahaany5673@gmail.com>
2023-07-16 14:04:23 +0300

    Last commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,21 @@
 import Image from "next/image"
 import Link from "next/link"
 import { Popover, Transition } from '@headlessui/react'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
+
+
+const services = [
+    { service: 'خدمه 1', url: '/' },
+    { service: 'خدمه 2', url: '/' },
+    { service: 'خدمه 3', url: '/' },
+    { service: 'خدمه 4', url: '/' },
+];
+
+const links = [
+    { link: 'الرئيسية', url: '/' },
+    { link: 'ماذا عنا', url: '/about' },
+    { link: 'خدماتنا', url: '/products' }
+]
 
 const Navbar = () => {
     return (
@@ -16,33 +31,30 @@ const Navbar = () => {
 
             {/* Desktop view of links */}
             <ul className="flex gap-8 max-sm:hidden">
-                <li className="text-1_Nav">
-                    <Link className="flexCenter gap-2 relative" href={'/products'}>
-                        <Popover>
-                            <Popover.Button className='flexCenter gap-2 outline-none border-none'>
-                                <span>
-                                    <Image src={'/vector.svg'} alt={'Arrow'} width={14} height={6.81} />
-                                </span>
-                                خدماتنا
-                            </Popover.Button>
-                            <Transition
-                                enter="transition ease-out duration-200"
-                                enterFrom="opacity-0 translate-y-1"
-                                enterTo="opacity-100 translate-y-0"
-                                leave="transition ease-in duration-150"
-                                leaveFrom="opacity-100 translate-y-0"
-                                leaveTo="opacity-0 translate-y-1"
-                            >
-                                <Popover.Panel className="absolute">
-                                    <div className="flex flex-col gap-4 mt-4 bg-white text-primary px-8 py-6 rounded-xl shadow">
-                                        <Link href="/" className="hover:bg-[#F3EAB3] py-2 px-14 rounded-2xl text-center">خدمة </Link>
-                                        <Link href="/about" className="hover:bg-[#F3EAB3] p-2 rounded-2xl text-center">خدمة 2</Link>
-                                        <Link href="/products" className="hover:bg-[#F3EAB3] p-2 rounded-2xl text-center">خدمة 3</Link>
-                                    </div>
-                                </Popover.Panel>
-                            </Transition>
-                        </Popover>
+                <li className="text-1_Nav flexCenter gap-2">
+
+                    <Menu>
+                        <MenuButton>
+                            <span>
+                                <Image src={'/vector.svg'} alt={'Arrow'} width={14} height={6.81} />
+                            </span>
+                        </MenuButton>
+                        <MenuList className="p-5 bg-secondary">
+                            <div>
+                                {services.map((serv) => (
+                                    <MenuItem className="hover:bg-white p-2 rounded-2xl text-center text-primary">
+                                        <Link href={serv.url} className="text-center w-full">
+                                            {serv.service}
+                                        </Link>
+                                    </MenuItem>
+                                ))}
+                            </div>
+                        </MenuList>
+                    </Menu>
+                    <Link href={'/products'}>
+                        خدماتنا
                     </Link>
+
                 </li>
                 <li className="text-1_Nav">
                     <Link href={'/about'}>
@@ -58,34 +70,27 @@ const Navbar = () => {
 
             {/* Mobile View of links */}
             <div className="max-sm:flex hidden">
-                <Popover className="relative">
-                    <Popover.Button className={'outline-none border-none'}>
+                <Menu>
+                    <MenuButton>
                         <Image src='/menu.svg' alt="menu icon" width={20} height={16} />
-                    </Popover.Button>
-                    <Transition
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                    >
-                        <Popover.Panel className="absolute z-999">
-                            <div className="flex flex-col gap-4 mt-4 bg-white text-primary p-6 rounded-xl shadow">
-                                <Link href="/" className="hover:bg-[#F3EAB3] p-2 rounded-2xl text-center">الرئيسية</Link>
-                                <Link href="/about" className="hover:bg-[#F3EAB3] p-2 rounded-2xl text-center">ماذا عنا</Link>
-                                <Link href="/products" className="hover:bg-[#F3EAB3] p-2 rounded-2xl text-center">خدماتنا</Link>
-                                <div>
-                                    <button className="customBtn gap-2 text_btn w-max !bg-[#F3EAB3] !text-primary relative">
-                                        <a href="tel:+2001123382824" className=" absolute top-0 w-full h-full" />
-                                        <Image src={'/phone.svg'} alt="Phone Icon" width={17} height={17} />
-                                        تواصل معنا
-                                    </button>
-                                </div>
-                            </div>
-                        </Popover.Panel>
-                    </Transition>
-                </Popover>
+                    </MenuButton>
+                    <MenuList className="p-5 bg-secondary">
+                        <div className="my-2">
+                            {links.map((each) => (
+                                <MenuItem className="hover:bg-white p-2 rounded-2xl text-center text-primary">
+                                    <Link href={each.url} className="text-center w-full">
+                                        {each.link}
+                                    </Link>
+                                </MenuItem>
+                            ))}
+                        </div>
+                        <button className="customBtn gap-2 bg-secondary text_btn relative w-full h-full">
+                            <a href="tel:+201009406638" className=" absolute top-0 w-full h-full" />
+                            <Image src={'/phone.svg'} alt="Phone Icon" width={17} height={17} />
+                            تواصل معنا
+                        </button>
+                    </MenuList>
+                </Menu>
             </div>
 
             <Link href={'/'}>
