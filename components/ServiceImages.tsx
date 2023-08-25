946ae2f72407bbd493341ca519f4cfc6f3ca9bb2
Ahmed Hany <ahaany5673@gmail.com>
2023-08-25 22:05:15 +0300

    third commit

--- components/ServiceImages.tsx
+++ components/ServiceImages.tsx
@@ -1,28 +1,22 @@
 'use client';
+
 import Image from 'next/image';
 
-const productImage = [
-  { source: '/product1.png' },
-  { source: '/product2.png' },
-  { source: '/product3.png' },
-  { source: '/product4.png' },
-];
-
 const ServiceImages = ({ data }: any) => {
-  console.log('🚀 ~ file: ServiceImages.tsx:14 ~ ServiceImages ~ data:', data);
   return (
     <div dir='rtl' className='p-6'>
       <h1 className='text-2 text-[20px] text-primary'>الصور للخدمة</h1>
       <div className='flex flex-wrap gap-6 py-3 flexCenter max-xl:justify-start max-lg:justify-center'>
-        {productImage.map((image, index) => (
-          <div key={index} className='bg-white rounded-xl'>
+        {data.test.pageData.productImage.map((image: any, index: any) => (
+          <div key={index} className='bg-white rounded-xl w-[300px] h-[300px]'>
             <Image
               src={image.source}
               alt='Product Image'
               width={300}
               height={300}
-              className='object-contain'
+              className='object-cover'
             />
+
           </div>
         ))}
       </div>
