0d8455af8316f66232fa4f6b04ad44558706ef44
Abdallah Ebrahim <abdalla.mezo@gmail.com>
2023-08-31 14:53:37 +0400

    fix: build issues
    Signed-off-by: Abdallah Ebrahim <abdalla.mezo@gmail.com>

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,23 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import React from 'react'
-import Location from './Location'
-import FooterInformation from './FooterInformation'
+import React from 'react';
+import FooterInformation from './FooterInformation';
+import dynamic from 'next/dynamic';
 
+const Location = dynamic(() => import('./Location'), {
+  ssr: false,
+});
 
 const Footer = () => {
   return (
-    <div className="p-6 bg-footer flex justify-between items-start flex-wrap max-md:gap-4" dir="rtl">
+    <div
+      className='p-6 bg-footer flex justify-between items-start flex-wrap max-md:gap-4'
+      dir='rtl'
+    >
       <FooterInformation />
       <Location />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Location.tsx
+++ components/Location.tsx
@@ -1,48 +1,62 @@
-'use client'
+'use client';
 
-import Image from 'next/image'
-import React from 'react'
-import { MapContainer, Marker, TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import { Icon } from 'leaflet'
+import Image from 'next/image';
+import React from 'react';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 
 const Location = () => {
-    return (
-        <div className="max-lg:w-full w-[60%] text-right text-white leading-loose">
-            <h1 className='text-2 text-[24px] '>مـوقـعـنـا</h1>
-            <p className='text-1 text-[20px] flex gap-4 max-xl:justify-start max-xl:items-baseline'>
+  return (
+    <div className='max-lg:w-full w-[60%] text-right text-white leading-loose'>
+      <h1 className='text-2 text-[24px] '>مـوقـعـنـا</h1>
+      <p className='text-1 text-[20px] flex gap-4 max-xl:justify-start max-xl:items-baseline'>
+        <Image
+          src={'/flag1.png'}
+          alt='Egypt Flag'
+          width={22}
+          height={22}
+          className='object-contain'
+        />
+        القاهرة - مدينة نصر - زهراء مدينة نصر - عمارة ۲۰۷۳ - شقة ٤ بعمارات ضباط
+        القوات المسلحة
+      </p>
+      <p className='text-1 text-[20px] flex justify-start items-baseline gap-4'>
+        <Image
+          src={'/flag2.png'}
+          alt='Saudi Flag'
+          width={25}
+          height={25}
+          className='object-contain'
+        />
+        الرياض - طريق الملك عبد العزيز الفرعي - حي العارض - شمال طريق الملك
+        سلمان ۲٥۲۹ - ٦٥١۳ - ١٣٣٤٥ / مكتب ٥
+      </p>
+      <div>
+        <MapContainer
+          className=' mt-4 w-[593px] h-[282px] max-md:!w-[350px] max-xs:!w-[250px] rounded-lg'
+          center={[30.0528718, 31.399776]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          />
+          <Marker
+            position={[30.0528718, 31.399776]}
+            icon={
+              new Icon({
+                iconUrl: '/location.png',
+                iconSize: [41, 41],
+                iconAnchor: [12, 41],
+              })
+            }
+          />
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
 
-                <Image
-                    src={'/flag1.png'}
-                    alt='Egypt Flag'
-                    width={22}
-                    height={22}
-                    className='object-contain'
-                />
-
-                القاهرة - مدينة نصر - زهراء مدينة نصر - عمارة ۲۰۷۳ - شقة ٤ بعمارات ضباط القوات المسلحة
-            </p>
-            <p className='text-1 text-[20px] flex justify-start items-baseline gap-4'>
-                <Image
-                    src={'/flag2.png'}
-                    alt='Saudi Flag'
-                    width={25}
-                    height={25}
-                    className='object-contain'
-                />
-                الرياض - طريق الملك عبد العزيز الفرعي - حي العارض - شمال طريق الملك سلمان ۲٥۲۹ - ٦٥١۳ - ١٣٣٤٥ / مكتب ٥
-            </p>
-            <div>
-                <MapContainer className=' mt-4 w-[593px] h-[282px] max-md:!w-[350px] max-xs:!w-[250px] rounded-lg' center={[30.0528718, 31.399776]} zoom={13} scrollWheelZoom={false}>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[30.0528718, 31.399776]} icon={new Icon({ iconUrl: '/location.png', iconSize: [41, 41], iconAnchor: [12, 41] })} />
-                </MapContainer>
-            </div>
-        </div>
-    )
-}
-
-export default Location
+export default Location;
